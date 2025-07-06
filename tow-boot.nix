0ad1d99e47978279c23e7b9bdb8320c8eb0490a4
Christoph Sarnowski <christoph@sarnowski.de>
2025-07-06 10:39:59 +0200

    use patched tow-boot to unlock flash before installing

--- tow-boot.nix
+++ tow-boot.nix
@@ -1,9 +1,9 @@
 let
-  tag = "release-2023.07-007";
-  sha256 = "sha256:1ffpx643nlxhs7x52rdaxwjx67wfl68rdy63qc8gsvlii3x3mskw";
+  commit = "fe5ba8a08a94bce5153f28bbd94abc2e1a3b91a3";
+  sha256 = "sha256:0yg4a0xim2g0qgi79am07vm9b44gv76mxmgc983cg1dm04mmg1jw";
   towBoot = builtins.fetchTarball {
     inherit sha256;
-    url = "https://github.com/tow-boot/tow-boot/archive/${tag}.tar.gz";
+    url = "https://github.com/csarn/tow-boot/archive/${commit}.tar.gz";
   };
 in {
   inherit towBoot;
